efc5514ecb273fe3643d77a1d094d1a519b5a658
Marek Strnad <mastrn@seznam.cz>
2024-04-15 12:56:12 +0200

    3

--- sorting.py
+++ sorting.py
@@ -22,23 +22,30 @@ def read_data(file_name):
                     data[header].append(int(value))
     return data
 
-def selection_sort(cisla):
+def selection_sort(cisla,direction):
     #první for cyklus si pamatuje indexy čísel
     for i in range (len(cisla)):
         min_index = i
         #druhý for cyklus si pamatuje minimum
-        for j in range(min_index+1, len(cisla)):
-            if cisla[j]< cisla[min_index]:
-                min_index = j
+        if direction == "vzestupně":
+            for j in range(min_index+1, len(cisla)):
+                if cisla[j]< cisla[min_index]:
+                    min_index = j
+        elif direction == "sestupně":
+            for j in range(min_index+1, len(cisla)):
+                if cisla[j]> cisla[min_index]:
+                    min_index = j
         #prohození čísel
         cisla[i], cisla[min_index] = cisla[min_index], cisla [i]
     return cisla
 
+#def bubble_sort(numbers):
 def main():
     data=read_data("numbers.csv")
     print(data)
-    sort = selection_sort(data["series_3"])
+    sort = selection_sort(data["series_1"],direction="sestupně")
     print(sort)
+    #sort2=bubble_sort(data["series_2"])
 
 
 if __name__ == '__main__':
