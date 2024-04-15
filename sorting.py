cff97fe4dd31acbabc611304883945317f0d9126
Marek Strnad <mastrn@seznam.cz>
2024-04-15 12:20:55 +0200

    1

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 def read_data(file_name):
@@ -10,10 +11,20 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
-
+    with open(file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data={}
+        for row in reader:
+            for header, value in row.items():
+                if header not in data:
+                    data[header] = [int(value)]
+                else:
+                    data[header].append(int(value))
+    return data
 
 def main():
-    pass
+    slovnik=read_data("numbers.csv")
+    print(slovnik)
 
 
 if __name__ == '__main__':
